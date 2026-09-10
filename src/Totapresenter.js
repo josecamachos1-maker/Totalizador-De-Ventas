a2a5186661f2b04c0e5245df096e04ad6638dd94
pochis <jose.camacho.s1@ucb.edu.bo>
2026-09-09 23:33:07 -0400

    Feat: Agregando html y funcionalidad de impuestos

--- src/Totapresenter.js
+++ src/Totapresenter.js
@@ -1,20 +1,33 @@
-import {Totalizador} from "./Tota.js";
+import { Totalizador } from "./Tota.js";
 
 const Producto = document.querySelector("#Producto-numero");
 const Cantidad = document.querySelector("#Cantidad-numero");
 const PrecioNeto = document.querySelector("#PrecioNeto-div");
+const Estado = document.querySelector("#Estado-codigo");
+const Impuesto = document.querySelector("#Impuesto-div");
 
-let totalizador = new Totalizador()
+let totalizador = new Totalizador();
 
-function mostrarPrecioNeto() {
+function mostrarResultados() {
 
   const precio = Number.parseFloat(Producto.value);
   const cantidad = Number.parseInt(Cantidad.value);
+  const codigo = Estado.value;
 
   if (!isNaN(precio) && !isNaN(cantidad)) {
-    PrecioNeto.value = totalizador.PrecioNeto(precio, cantidad);
+
+    const neto = totalizador.PrecioNeto(precio, cantidad);
+
+    PrecioNeto.value = neto.toFixed(2);
+
+    const impuestoCalculado = totalizador.Impuesto(codigo, neto);
+
+    Impuesto.value = impuestoCalculado.toFixed(2);
+
   }
+
 }
 
-Producto.addEventListener("input", mostrarPrecioNeto);
-Cantidad.addEventListener("input", mostrarPrecioNeto);
+Producto.addEventListener("input", mostrarResultados);
+Cantidad.addEventListener("input", mostrarResultados);
+Estado.addEventListener("change", mostrarResultados);
